53d635a7877fccaa2badeccab075cc35ca264fb0
Vasile Gorcinschi <vasigorc@gmail.com>
2024-11-29 19:22:21 -0500

    chore: Added second model

--- src/apis/call_requests.rs
+++ src/apis/call_requests.rs
@@ -1,6 +1,9 @@
 use crate::models::general::llm::Message;
 use dotenv::dotenv;
-use reqwest::Client;
+use reqwest::{
+    header::{HeaderMap, HeaderValue},
+    Client,
+};
 use std::env;
 
 pub async fn call_gpt(message: Vec<Message>) {
@@ -13,4 +16,18 @@ pub async fn call_gpt(message: Vec<Message>) {
         env::var("OPEN_API_ORG").expect("OPEN_API_ORG not found among environment variables");
 
     let url: &str = "https://api.openapi.com/v1/chat/completions";
+
+    // Create API key header
+    let mut headers = HeaderMap::new();
+    headers.insert(
+        "authorization",
+        HeaderValue::from_str(&format!("Bearer {}", api_key)).unwrap(),
+    );
+    headers.insert(
+        "OpenAI-Organization",
+        HeaderValue::from_str(api_org.as_str()).unwrap(),
+    );
+
+    // Create client
+    let client = Client::builder().default_headers(headers).build().unwrap();
 }
