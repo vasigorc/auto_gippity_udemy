ef18fd711ab5e1b8f45a57f7be686f5de8968c63
Vasile Gorcinschi <vasigorc@users.noreply.github.com>
2024-12-13 01:00:36 -0500

    chore: Started SpecialFunctions for AgentSolutionArchitect

--- src/models/agents/agent_architect.rs
+++ src/models/agents/agent_architect.rs
@@ -3,8 +3,9 @@ use crate::{
     helpers::general::ai_task_request_decoded,
     models::agent_basic::basic_agent::{AgentState, BasicAgent},
 };
+use async_trait::async_trait;
 
-use super::agent_traits::{FactSheet, ProjectScope};
+use super::agent_traits::{FactSheet, ProjectScope, SpecifalFunctions};
 
 #[derive(Debug)]
 pub struct AgentSolutionArchitect {
@@ -12,6 +13,7 @@ pub struct AgentSolutionArchitect {
 }
 
 impl AgentSolutionArchitect {
+    #[allow(dead_code)]
     pub fn new() -> Self {
         let attributes = BasicAgent {
             // go to previous location g; go to next location g,
@@ -59,5 +61,36 @@ impl AgentSolutionArchitect {
 
         fact_sheet.external_urls = ai_response;
         self.attributes.state = AgentState::UnitTesting;
+    }
+}
+
+#[async_trait]
+impl SpecifalFunctions for AgentSolutionArchitect {
+    fn get_attributes_from_agent(&self) -> &BasicAgent {
+        &self.attributes
+    }
+
+    async fn execute(
+        &mut self,
+        fact_sheet: &mut FactSheet,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        while self.attributes.state != AgentState::Finished {
+            match self.attributes.state {
+                AgentState::Discovering => {
+                    let project_scope = self.call_project_scope(fact_sheet).await;
+                    if project_scope.is_external_urls_required {
+                        self.call_determine_external_urls(
+                            fact_sheet,
+                            fact_sheet.project_description.clone(),
+                        )
+                        .await;
+                        self.attributes.state = AgentState::UnitTesting;
+                    }
+                }
+                AgentState::UnitTesting => self.attributes.state = AgentState::Finished,
+                _ => self.attributes.state = AgentState::Finished,
+            }
+        }
+        Ok(())
     }
 }
